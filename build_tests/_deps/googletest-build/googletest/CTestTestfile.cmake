3c27ad327a10cb27ea7fb1633d1fd3b3054f5b1d
Thangam <your.thagamveeraputhiran2004@gmail.com>
2026-07-30 12:34:50 +0530

    Remove build folders from repo, add to gitignore

--- build_tests/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thanga-inc-5829/Documents/Infusion_pump/build_tests/_deps/googletest-src/googletest
-# Build directory: /home/thanga-inc-5829/Documents/Infusion_pump/build_tests/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
